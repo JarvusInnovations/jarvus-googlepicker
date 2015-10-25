bd790c5774c8885767f16ea5a7a257491a62496b
Bill Clinton <BillClinton@users.noreply.github.com>
2015-10-25 02:01:07 -0400

    Include google api for authorization

--- src/util/GooglePickerApi.js
+++ src/util/GooglePickerApi.js
@@ -2,6 +2,14 @@
 Ext.define('Jarvus.util.GooglePickerApi', {
     extend: 'Ext.ux.google.Api',
 
+    mixins: [
+        'Ext.mixin.Mashup'
+    ],
+
+    requiredScripts: [
+        '//apis.google.com/js/api.js'
+    ],
+
     requiresGoogle: [
         'picker'
     ]
